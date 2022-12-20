a83a4080fbc37511c40b5b6f25d100af8c7e2b09
Matias-Estay <matias.estay@hotmail.com>
2022-12-20 04:31:18 -0300

    cambio de color en toolbar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,10 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar class="bg-black text-white">
 
         <q-toolbar-title>
-          Quasar App
+          Portafolios
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -20,11 +20,6 @@
 <script>
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'MainLayout',
-  setup () {
-    return {
-
-    }
-  }
+  name: 'MainLayout'
 })
 </script>
