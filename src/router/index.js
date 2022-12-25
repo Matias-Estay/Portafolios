0e52ebc2319a606225842161a7a6fbc9e2118e0b
Matias-Estay <matias.estay@hotmail.com>
2022-12-25 19:04:41 -0300

    corrección inicio

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
-
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
@@ -24,7 +20,7 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE)
+    history: createWebHistory('/')
   })
 
   return Router
