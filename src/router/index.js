52bfa7125c4e49a64418818c77a0c4d9eda448f0
Matias-Estay <matias.estay@hotmail.com>
2022-12-25 19:05:16 -0300

    librerias sin uso

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 import '../css/app.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
